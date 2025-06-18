bbaf37470da8d24b2149123770d82e6ce3088518
starlyx <reyesabranlyalyx@gmail.com>
2025-06-18 09:38:00 +0200

    Agregar lógica de inicio de sesión y archivos de configuración

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import tkinter as tk 
+from tkinter import ttk
 
 root= tk.Tk()
 root.title("Login")
 
-
+labelWelcome = tk.Label(root, text="Bienvenido al sistema de login", font=("Arial", 16))
 
 root.mainloop()
